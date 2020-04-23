0be09e4a1188235d76c6b3830be734232de65ac6
Ferranupf <59828147+Ferranupf@users.noreply.github.com>
2020-04-23 05:14:05 -0700

    Redone following  raywenderlich's guide

---
+++ Tests and Exercises/Scripting Practice (Fine)/Assets/RW/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector3 movementSpeed; //1
+    public Space space; //2
+
+    void Start()
+    {
+        movementSpeed = new Vector3(0,0,20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementSpeed * Time.deltaTime, space);
+    }
+}
