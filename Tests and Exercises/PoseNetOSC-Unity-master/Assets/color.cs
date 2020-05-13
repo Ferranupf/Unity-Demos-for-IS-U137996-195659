ce52d30bf375cbf50fcca700392348ed60ac1e04
Ferranupf <59828147+Ferranupf@users.noreply.github.com>
2020-05-13 00:08:12 -0700

    Commited P3 after having issues with the cam and node.js bridge/detection

---
+++ Tests and Exercises/PoseNetOSC-Unity-master/Assets/color.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Ball;
+    public Material NewColor;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ball.transform.position.x != 0)
+            Ball.GetComponent<MeshRenderer>().material = NewColor;
+
+
+    }
+}
